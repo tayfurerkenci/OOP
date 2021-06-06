241aeac8e9cce1485e875b5206081705ce23454f
Tayfur Erkenci <terkenci@gmail.com>
2021-06-06 11:59:47 +0300

    Microsoft Official Tutorial has been completed  for CSharp OOP

---
+++ ShowCase/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ShowCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Tayfur", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} $ initial balance.");
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+            // Test that the initial balances must be positive.
+            // BankAccount invalidAccount;
+            // try
+            // {
+            //     invalidAccount = new BankAccount("invalid", -55);
+            // }
+            // catch (ArgumentOutOfRangeException e)
+            // {
+            //     Console.WriteLine("Exception caught creating account with negative balance");
+            //     Console.WriteLine(e.ToString());
+            //     return;
+            // }
+            // // Test for a negative balance.
+            // try
+            // {
+            //     account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+            // }
+            // catch (InvalidOperationException e)
+            // {
+            //     Console.WriteLine("Exception caught trying to overdraw");
+            //     Console.WriteLine(e.ToString());
+            // }
+            var lineOfCredit = new LineOfCreditAccount("line of credit", 0, 2000);
+            // How much is too much to borrow?
+            lineOfCredit.MakeWithdrawal(1000m, DateTime.Now, "Take out monthly advance");
+            lineOfCredit.MakeDeposit(50m, DateTime.Now, "Pay back small amount");
+            lineOfCredit.MakeWithdrawal(5000m, DateTime.Now, "Emergency funds for repairs");
+            lineOfCredit.MakeDeposit(150m, DateTime.Now, "Partial restoration on repairs");
+            lineOfCredit.PerformMonthEndTransactions();
+            Console.WriteLine(lineOfCredit.GetAccountHistory());
+
+        }
+    }
+}
